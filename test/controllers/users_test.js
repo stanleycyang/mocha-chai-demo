446f34b64cc7f086749a9acb2f0da8b9fbbcfaf4
syang019 <stanley@stanleycyang.com>
2015-08-07 10:53:23 -0700

    Added npm test command

--- test/controllers/users_test.js
+++ test/controllers/users_test.js
@@ -1,10 +1,24 @@
 var expect = require('chai').expect;
 
 describe('UsersController', function(){
-    describe('#index', function(){});
-    describe('#show', function(){});
-    describe('#create', function(){});
-    describe('#update', function(){});
-    describe('#destroy', function(){});
+    describe('#index', function(){
+    
+    });
+
+    describe('#show', function(){
+    
+    });
+
+    describe('#create', function(){
+    
+    });
+
+    describe('#update', function(){
+    
+    });
+
+    describe('#destroy', function(){
+    
+    });
 });
 
---
+++ test/models/user_test.js
@@ -0,0 +1,9 @@
+var mocha = require('mocha'),
+    expect = require('chai').expect,
+    User = require('../../app/models/User');
+
+describe('User', function(){
+    it('exists', function(){
+        expect(user).to.exist;
+    });
+});
