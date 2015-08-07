68067519d66646238fb124537db3711dba16e99c
syang019 <stanley@stanleycyang.com>
2015-08-07 10:42:28 -0700

    Added sinon module

---
+++ test/controllers/users_test.js
@@ -0,0 +1,10 @@
+var expect = require('chai').expect;
+
+describe('UsersController', function(){
+    describe('#index', function(){});
+    describe('#show', function(){});
+    describe('#create', function(){});
+    describe('#update', function(){});
+    describe('#destroy', function(){});
+});
+
