635661db5d1104c703da97b1b6135a629a117866
syang019 <stanley@stanleycyang.com>
2015-08-07 22:24:52 -0700

    Added user model tests

--- test/models/user_test.js
+++ test/models/user_test.js
@@ -1,9 +1,53 @@
 var mocha = require('mocha'),
     expect = require('chai').expect,
-    User = require('../../app/models/User');
+    mockgoose = require('mockgoose'),
+    mongoose = require('mongoose'),
+    User = require('../../app/models/User'),
+    mockUser;
+
+mockgoose(mongoose);
 
 describe('User', function(){
+
+    // See if the model exists
     it('exists', function(){
-        expect(user).to.exist;
+        expect(User).to.exist;
     });
+
+    it('is a constructor', function(){
+        expect(typeof User).to.equal('function');
+    });
+
+    it('is a model with full suite of functions', function(){
+        expect(User.find).to.exist;
+        expect(typeof User.find).to.equal('function');
+
+        expect(User.findOne).to.exist;
+        expect(typeof User.findOne).to.equal('function');
+
+        expect(User.remove).to.exist;
+        expect(typeof User.remove).to.equal('function');
+
+        expect(User.count).to.exist;
+        expect(typeof User.count).to.equal('function');
+
+        expect(User.findById).to.exist;
+        expect(typeof User.findById).to.equal('function');
+
+        expect(User.findByIdAndRemove).to.exist;
+        expect(typeof User.findByIdAndRemove).to.equal('function');
+
+        expect(User.findByIdAndUpdate).to.exist;
+        expect(typeof User.findByIdAndUpdate).to.equal('function');
+
+        expect(User.findOneAndRemove).to.exist;
+        expect(typeof User.findOneAndRemove).to.equal('function');
+
+        expect(User.update).to.exist;
+        expect(typeof User.update).to.equal('function');
+           
+    });
+
+
+
 });
--- test/routes/user_test.js
+++ test/routes/user_test.js
@@ -3,73 +3,74 @@ var should = require('chai').should(),
     supertest = require('supertest'),
     api = supertest('http://localhost:3000');
 
-describe('User', function(){
-    describe('#index', function(){
-        it('returns a 200 response', function(done){
-            api.get('/users')
-               .set('Accept', 'application/json')
-               .expect(200, done);
-        });
 
-        it('is an object with keys and values', function(done){
-            api.get('/users')
-               .set('Accept', 'application/json')
-               .expect(200)
-               .end(function(error, response){
-                    expect(response.body).to.be.an.instanceof(Array);
-                    expect(response.body[0]).to.have.property('name');
-                    expect(response.body[0].name).to.not.equal(null);
-                    done();
-               });
-        });
-    });
+//describe('User', function(){
+    //describe('#index', function(){
+        //it('returns a 200 response', function(done){
+            //api.get('/users')
+               //.set('Accept', 'application/json')
+               //.expect(200, done);
+        //});
 
-    describe('#create', function(){
-        before(function(done){
-            api.post('/users')
-                .set('Accept', 'application/x-www-form-urlencoded')
-                .send({
-                    name: 'Stanley'
-                })
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end(function(error, response){
-                    var user = response;
-                    done();
-                });
-        });
-        it('returns a new user', function(){
-            api.post('/users')
-               .set('Accept', 'application/x-www-form-urlencoded')
-               .send({
-                    name: 'Stanley'
-               })
-               .expect(200)
-               .end(function(error, response){
-                    expect(response.body.name).to.equal('Stanley');
-                    expect(response.body).to.be.an.instanceof(Object);
-                    done();
-               });
-        });
-    });
+        //it('is an object with keys and values', function(done){
+            //api.get('/users')
+               //.set('Accept', 'application/json')
+               //.expect(200)
+               //.end(function(error, response){
+                    //expect(response.body).to.be.an.instanceof(Array);
+                    //expect(response.body[0]).to.have.property('name');
+                    //expect(response.body[0].name).to.not.equal(null);
+                    //done();
+               //});
+        //});
+    //});
 
-    describe('#update', function(){
-        it('is updated with a new name', function(){
-            api.put('/users/55c3f53c070020e4c92898ee')
-                .set('Accept', 'application/x-www-form-urlencoded')
-                .send({
-                    name: 'Sebastian'
-                })
-                .expect(200)
-                .end(function(error, response){
-                    expect(response.body.name).to.equal('Sebastian');
-                    expect(response.body).to.be.an.instanceof(Object);
-                    done();
-                });
-        });
-    });
+    //describe('#create', function(){
+        //before(function(done){
+            //api.post('/users')
+                //.set('Accept', 'application/x-www-form-urlencoded')
+                //.send({
+                    //name: 'Stanley'
+                //})
+                //.expect('Content-Type', /json/)
+                //.expect(200)
+                //.end(function(error, response){
+                    //var user = response;
+                    //done();
+                //});
+        //});
+        //it('returns a new user', function(){
+            //api.post('/users')
+               //.set('Accept', 'application/x-www-form-urlencoded')
+               //.send({
+                    //name: 'Stanley'
+               //})
+               //.expect(200)
+               //.end(function(error, response){
+                    //expect(response.body.name).to.equal('Stanley');
+                    //expect(response.body).to.be.an.instanceof(Object);
+                    //done();
+               //});
+        //});
+    //});
 
-});
+    //describe('#update', function(){
+        //it('is updated with a new name', function(){
+            //api.put('/users/55c3f53c070020e4c92898ee')
+                //.set('Accept', 'application/x-www-form-urlencoded')
+                //.send({
+                    //name: 'Sebastian'
+                //})
+                //.expect(200)
+                //.end(function(error, response){
+                    //expect(response.body.name).to.equal('Sebastian');
+                    //expect(response.body).to.be.an.instanceof(Object);
+                    //done();
+                //});
+        //});
+    //});
+
+//});
 
 
 
