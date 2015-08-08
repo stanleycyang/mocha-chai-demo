91f2cc6cd95d17c6bd91acd9fb5231fbb6033c04
syang019 <stanley@stanleycyang.com>
2015-08-07 22:44:52 -0700

    modularized model tests

--- test/models/user_test.js
+++ test/models/user_test.js
@@ -18,34 +18,49 @@ describe('User', function(){
         expect(typeof User).to.equal('function');
     });
 
-    it('is a model with full suite of functions', function(){
-        expect(User.find).to.exist;
-        expect(typeof User.find).to.equal('function');
-
+    it('has the findOne functionality', function(){
         expect(User.findOne).to.exist;
         expect(typeof User.findOne).to.equal('function');
+    });
 
+    it('has the remove functionality', function(){
         expect(User.remove).to.exist;
         expect(typeof User.remove).to.equal('function');
+    });
 
+    it('has the count functionality', function(){
         expect(User.count).to.exist;
         expect(typeof User.count).to.equal('function');
+    });
 
+    it('has the findById functionality', function(){
         expect(User.findById).to.exist;
         expect(typeof User.findById).to.equal('function');
+    });
 
+    it('has the findByIdAndRemove functionality', function(){
         expect(User.findByIdAndRemove).to.exist;
         expect(typeof User.findByIdAndRemove).to.equal('function');
+    });
 
+    it('has the findByIdAndUpdate functionality', function(){
         expect(User.findByIdAndUpdate).to.exist;
         expect(typeof User.findByIdAndUpdate).to.equal('function');
+    });
 
+    it('has the findOneAndRemove functionality', function(){
         expect(User.findOneAndRemove).to.exist;
         expect(typeof User.findOneAndRemove).to.equal('function');
+    });
 
+    it('has the update functionality', function(){
         expect(User.update).to.exist;
         expect(typeof User.update).to.equal('function');
-           
+    });
+
+    it('has the find functionality', function(){
+        expect(User.find).to.exist;
+        expect(typeof User.find).to.equal('function');
     });
 
 
